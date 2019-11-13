eeaad6385da615e5ad9c2f8fca2c1101ee9c2b6a
cahila23 <alexander.cahill@mail.dcu.ie>
2019-11-13 13:08:35 +0000

    updated server.js so html file is properly displayed, added game.js and fixed index.html.

--- src/server.js
+++ src/server.js
@@ -12,14 +12,24 @@ var io = socketIO(server);
 const PORT = 3000;
 
 app.set("port", PORT); //set port to 3000 for now
+
 app.use("/static", express.static(__dirname + "/static")); //for routing
 
 
 app.get("/", function(req, res) {
   //sends html file stored in 'index.html' when endpoint is hit
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile("index.html", {root: path.join(__dirname, "./static")});
 });
 
 server.listen(PORT, function() {
   console.log("server started on port %d", PORT);
-})
+});
+
+//websocket handler
+io.on("connection", function(socket) {
+});
+
+//used for testing, emits a "yo" every 1000ms
+setInterval(function() {
+io.sockets.emit("msg", "yo");
+}, 1000);
