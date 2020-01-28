f36b6f58afb8f1839a9e214152f529b8532f3b50
cahila23 <alexander.cahill@mail.dcu.ie>
2020-01-28 17:58:35 +0000

    moved javascript out of play.html -> game.js>

--- src/static/game.js
+++ src/static/game.js
@@ -1,15 +1,37 @@
 var socket = io();
+socket.emit("newClient");
 
-socket.on("message", function(data) {
-    console.log(data);
+
+
+var clientX = 0;
+var clientY = 0;
+window.addEventListener("mousemove", event => {
+    clientX = event.x;
+    clientY = event.y;
+    console.log( `x: ${clientX} | y: ${clientY}`);
 });
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
+camera.position.z = 7;
+clientX = 0.0
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
 
-window.addEventListener("mousemove", event => {
-    console.log( `x: ${event.x} | y: ${event.y}`)
-})
-
-socket.emit("newClient");
+  //cube.translateX(clientX);
+  //console.log("animation req");
+}
+animate();
