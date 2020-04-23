df7e5f20bb02e5f26d414bbdfce870f3ed474f31
ocearbl2 <liam.ocearbhaill2@mail.dcu.ie>
2020-04-23 14:59:12 +0100

    Added some tests. I notice that socket-tester has issues with assertions. I got in contact with the author of the library and he was extremely unhelpful. Modifying code where I can to avoid having to use socket-tester from now on

--- test/static/matchmake.spec.js
+++ test/static/matchmake.spec.js
@@ -6,7 +6,7 @@ var app = require('../../src/server');
 
 var socketUrl = 'http://localhost:80';
 
-var options = {  
+var options = {
     transports: ['websocket'],
     'force new connection': true,
     'forceNew': true
@@ -14,38 +14,38 @@ var options = {
 
 var room = 'lobby';
 
-describe('Matchmake test', function () {  
+describe('Matchmake test', function () {
     var client1, client2, client3;
-  
+
     // testing goodness goes here
-    it('should send and receive a message(autoMatch)', function (done) {  
+    it('should send and receive a message(autoMatch)', function (done) {
         // Set up client1 connection
         client1 = io.connect(socketUrl, options);
-    
+
         // Set up event listener.  This is the actual test we're running
         client1.on('message', function(msg){
           expect(msg).to.equal('autoMatch');
-    
+
           // Disconnect both client connections
           client1.disconnect();
           client2.disconnect();
           done();
         });
-    
+
         client1.on('connect', function(){
           client1.emit('join room', room);
-    
+
           // Set up client2 connection
           client2 = io.connect(socketUrl, options);
-    
+
           client2.on('connect', function(){
-    
+
             // Emit event when all clients are connected.
             client2.emit('join room', room);
             client2.emit('message', 'autoMatch');
             done();
           });
-    
+
         });
       });
   });
